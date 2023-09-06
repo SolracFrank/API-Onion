23513eec0c54b86f9a4ebc90b6709eba8a049932
Francisco Herrera <francisco.herrrera@proan.com>
2023-09-06 10:14:18 -0600

    Initial structure

---
+++ BancoAPI/Application/DependencyInjections.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+namespace Application
+{
+    public static class DependencyInjections
+    {
+        public static void AddApplication (this IServiceCollection services)
+        {
+            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+    
